55b14832e7210d0bc4c78b83a1f5e1ca82bb6310
mrghost99 <40465924+mrghost99@users.noreply.github.com>
2018-06-21 21:41:31 +0100

    source and exe

--- duel all in 1.cpp
+++ duel all in 1.cpp
@@ -4,11 +4,11 @@ void ds(int q)
 {
     // reading //
     int a,b,c,i,armor1,armor2,A,B,hp1,hp2;c=0;int d=1;int A1=0;int d2=0;int sabbath=0;int sb=0;int str=5;int d3=0;int d4=0;int red1=0;int red2=0;int cond1=1;int cond2=1;
-        printf("\ngive 'A's power= ");
+        printf("\ngive first attacking unit  power= ");
           scanf(" %i",&a);
           printf("\ngive armor= ");
           scanf(" %i",&armor1);
-          printf("\nis it sabbath? yes=1 no=other #=");
+          printf("\nis it this unit sabbath? yes=1 no=other #=");
           scanf("%d",&sb);
           if(sb==1)
               {
@@ -21,27 +21,31 @@ void ds(int q)
                   scanf(" %i",&d2);
                   printf("\nis this unit a Redanian Elite? yes=1 , no=other ,   #= ");
                   scanf("%d",&d3);
+                  if(armor1==0)
+                  {
+                      d3=0;
+                  }
               }
-
             printf("\ndueling Redanian Elite? yes=1 , no=other ,   #= ");
             scanf(" %i",&d4);
-            if(d3==1)
+            if(d3==1) // A redanian elite
             {
             red1=5;
             }
-            if(d4==1) // A
+            if(d4==1) // B redanian elite
             {
             red2=5;
             }
-          printf("\ngive 'B's= ");
+          printf("\ngive second attacking unit power= ");
           scanf(" %i",&b);
           printf("\ngive armor= ");
           scanf(" %d",&armor2);
-          hp1=a+armor1;
+          printf("\n RESULTS: \n");
+          hp1=a+armor1; // defiening units health and saving their powers to calculate results
           hp2=b+armor2;
           B=b;
           A=a;
-          if(d2==1)
+          if(d2==1) // double dammage
           {
               d=2;
           }
@@ -138,7 +142,7 @@ void hduel(int k)
         c=0;
         int AR2=0;
         int ign=0;
-        printf("\ngive your units details \n  ");
+        printf("\ngive the *SECOND* attacking unit details \n  ");
         printf("give power= ");
         scanf(" %i",&b);
         printf("\n give armor= ");
@@ -146,7 +150,7 @@ void hduel(int k)
         printf("\nis this unit a Redanian Elite? yes=1 , no=other ,   #= ");
         scanf("%d",&d3);
         hp2=b+armor2;B=b;AR2=armor2;i=0;
-        printf("\ndueling Redanian Elite?( if yes it only shows redanian elites value ) yes=1 , no=other ,   #= ");
+        printf("\ndueling Redanian Elite?( if yes it only shows redanian elites values ) yes=1 , no=other ,   #= ");
         scanf(" %i",&d4);
         if(d3==1) // B
         {
@@ -171,7 +175,7 @@ void hduel(int k)
 
                         b=B;c=0;a=A;armor2=AR2;hp2=B+AR2;
                         hp1=a+g;
-                        armor1=g;cond2=1;d3=D3;cond1=1;d4=D4;
+                        armor1=g;cond2=1;cond1=1;
                         // duel calc //
                         while(hp1>0 && hp2>0)
                             {
@@ -198,7 +202,7 @@ void hduel(int k)
                                      {
                                         armor2=0;
                                         b=hp2;
-                                        if(d3==1 && b>0 && cond2==1)
+                                        if(d3==1 && b>0 && cond2==1) // redElits
                                         {
                                             b=b+red2;
                                             hp2=b;
@@ -221,7 +225,7 @@ void hduel(int k)
                                      {
                                          a=hp1;
                                          armor1=0;
-                                         if(d4==1 && a>0 && cond1==1)
+                                         if(d4==1 && a>0 && cond1==1) // redElits
                                             {
                                                 a=hp1+red1;
                                                 hp1=a;
@@ -245,8 +249,8 @@ void hduel(int k)
                                     v2[s]=g;
                                     if(d4==1 && g==0) // to ignore non armor units for redanian elites
                                     {
-                                        v4[s]=0;
-                                        INv=0;
+                                        v4[s]=-1;
+                                        INv=-1;
                                     }
                                     v1[s]=i;
                                     s=s+1;
@@ -261,7 +265,7 @@ void hduel(int k)
 
                 }
                 // giving results //
-                ign=0;
+                ign=0; // whether to ignore armored units
                 if(d4!=1)
                 {
                     printf("\nignore armor? yes=1 no=other, #=");
@@ -277,19 +281,19 @@ void hduel(int k)
                         }
                     }
                 }
-                int n;int o1,o2;
-                printf("\n here's the best units to duel for value = %d, to %d (power,armor) :",INvalue,INvalue-(B%10)-(B/10));
-                float x=(B%10)+(B/10)+1;
+                int n;int o1,o2; // formatting variables
+                printf("\n here's the best units to duel for value = %d, to %d (power,armor) :",INvalue,INvalue-(B%10)-(B/10)-5); // changes search range based on unit's power
+                float x=(B%10)+(B/10)+5+1; // changes search range based on unit's power
                 printf("\n");
                 for(n=0;n<x;n++)
                 {
                     o1=0;o2=0;
                     for(w=0;w<s+1;w++)
                     {
-                        if(v4[w]==INvalue-n)
+                        if(v4[w]==INvalue-n && v4[w]>=0)
                         {
                             o2+=1;
-                            if(o1==0) //to print a single if
+                            if(o1==0) //to print a single "PTS" msg
                             {
                                 printf(" %d PTS",v4[w]);
                                 o1+=1; // to break if statement
@@ -331,22 +335,22 @@ void hduel(int k)
 void mduel(int k)
 {
     // reading //
-               int a,b,armor1,armor2,i,g,hp1,hp2,A,B,INvalue,INv,OUTvalue,OUTv,c,INar2,OUTar2;int AR1=0;int s=0; int j=0;int w=0;int d2=0;int d=1;int d3=0;int red=0;
+               int a,b,armor1,armor2,i,g,hp1,hp2,A,B,INvalue,INv,c,INar2;int AR1=0;int s=0; int j=0;int w=0;int d2=0;int d=1;int d3=0;int red=0;
                 INvalue=0;
                 INar2=0;
-                OUTvalue=0;int str=5;
+                int str=5; // sabbath
                 c=0;
                 int sb=0;int sabbath=0;int cond=1;
-              printf("\ngive your units details \n  ");
+              printf("\ngive the FIRST attacking unit details \n  ");
               printf("give power= ");
               scanf(" %i",&a);
               printf("\n give armor= ");
               scanf(" %i",&armor1);
-              printf("\nis it sabbath? yes=1 no=other #=");
+              printf("\nis this unit sabbath? yes=1 no=other #=");
               scanf("%d",&sb);
               if(sb==1)
               {
-                  printf("\nit's strength (default=5) ? =");
+                  printf("\nit's strength (default is 5) ? =");
                   scanf("%d",&str);
               }
               if(sb!=1)
@@ -364,10 +368,9 @@ void mduel(int k)
               {
                   d=2;
               }
-              hp1=a+armor1;A=a;AR1=armor1;i=0;
+              hp1=a+armor1;A=a;AR1=armor1;i=0; // defining powers and saving power for future reset
               long int v1[150000];long int v2[150000];long int v3[150000];
               // changing power while saving B's power //
-              clock_t begin = clock();
               for(i=1;i<2*d*A*5;i++)
               {
                   b=i;B=b;
@@ -376,11 +379,11 @@ void mduel(int k)
                   for(g=0;g<=10;g++)
                   {
 
-
+                        // resetting all necessary values
                         b=B;c=0;a=A;armor1=AR1;hp1=a+armor1;
                         hp2=b+g;
                         armor2=g;sabbath=0;cond=1;
-
+                        // duel calc
                         while(hp1>0 && hp2>0)
                             {
                                  c=c+1;
@@ -406,7 +409,7 @@ void mduel(int k)
                                      {
                                         armor2=0;
                                         b=hp2;
-                                        if(d3==1 && b>0 && cond==1)
+                                        if(d3==1 && b>0 && cond==1) // RedElits
                                             {
                                             b+=red;
                                             hp2=b;
@@ -433,7 +436,7 @@ void mduel(int k)
                                  if(b<=0)
                                      {
                                          b=0;
-                                         if(a<str)
+                                         if(a<str) // sabbath
                                          {
                                             sabbath=1;
                                              if (a<str-1)
@@ -459,10 +462,10 @@ void mduel(int k)
                                     }
                                     v3[j]=INv;
                                     v2[j]=g;
-                                    if(d3==1 && g==0)
+                                    if(d3==1 && g==0) // ignore non armored units if dueling RE
                                     {
-                                        v3[j]=0;
-                                        INv=0;
+                                        v3[j]=-1;
+                                        INv=-1;
                                     }
                                     v1[j]=i;
                                     j=j+1;
@@ -495,9 +498,9 @@ void mduel(int k)
                         }
                     }
                 }
-                int n;int o1,o2;
-                printf("\n here's the best units to duel for value = %d, to %d (power,armor) :",INvalue,INvalue-(A%10)-(A/10));
-                float x=(A%10)+(A/10)+1;
+                int n;int o1,o2; // formatting variables
+                printf("\n here's the best units to duel for value = %d, to %d (power,armor) :",INvalue,INvalue-(A%10)-(A/10)-5); // changes search range based on unit's power
+                float x=(A%10)+(A/10)+5+1; // changes search range based on unit's power
                 printf("\n\n");
                 for(n=0;n<x;n++)
                 {
@@ -514,7 +517,7 @@ void mduel(int k)
                             }
                             if(o2%6==0 && o2!=0) // formatting : return to line every 5 cases
                             {
-                                if(v3[w]>=100)
+                                if(v3[w]>=100) // makes the parentheses on same line
                                 {
                                     printf("\n        ");
                                     o2+=1; // so the mod only goes to a new line every 5 cases
@@ -538,17 +541,7 @@ void mduel(int k)
                         printf("\n\n");
                     }
 
-
-
-
                 }
-                clock_t end = clock();
-                double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-                printf("\ntime spent=%f",time_spent);
-
-
-
-
 
 }
 
@@ -560,10 +553,10 @@ int main()
         int y=1;int K=0;int L=0;int Q=0;
         while(y==1)
         {
-            printf("calculate duel ( 1 ) or search for best value? ( 2 ) , exit = other, #= ");
+            printf("calculate duel ( 1 ) or search for best units to duel? ( 2 ) , exit = other, #= ");
             scanf("%d",&Q);
             L=Q;
-            while(Q==1 || L==1)
+            while(Q==1 || L==1) // "L" is to eliminate ambiguities between the two modes
                 {
                     ds(L);
                     printf("\n+++++++++++++++++++++++++++++++\n");
